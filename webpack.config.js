04bff85eb7f68e4195bb32e4130328bbc59d8516
Pauleg7 <p.s.84@mail.ru>
2019-03-06 21:40:04 +0300

    add task_2 in modules

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path'); 
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+  },
+};
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+  ]
+};
